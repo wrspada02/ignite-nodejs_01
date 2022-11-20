1766a08028501e5409763c4b6c5289b4946df488
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-11-20 15:34:14 -0300

    feat: create endpoints

--- src/index.js
+++ src/index.js
@@ -1,41 +1,95 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const isUserAlreadyExists = users.find(user => user.username === username);
+
+  if(!isUserAlreadyExists) {
+    response.status(400).json({  error: 'Mensagem do erro' });
+  }
+
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const isUserAlreadyExists = users.find(user => user.username === username);
+
+  if(!isUserAlreadyExists) {
+    response.status(400).json({ error: "User with this username already exists" });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name, 
+    username,
+    todos: []
+  };
+
+  users.push(newUser);
+
+  response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+
+  const newTodo = {
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  const user = users.find(user => user.username === username);
+
+  user.todos.push(newTodo);
+
+  response.status(201).json(newTodo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const taskId = request.params.id;
+  const { username } = request.headers;
+  const { title, deadline } = request.body;
+
+  response.status(200).send();
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  const taskId = request.params.id;
+  
+  response.status(200).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const taskId = request.params.id;
+  const { username } = request.headers;
+
+  response.status(200).send();
 });
 
 module.exports = app;
