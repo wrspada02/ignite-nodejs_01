ab12488da18c7c54964de9c98c649c583f470cc4
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-11-24 22:29:21 -0300

    chore: put, patch works

--- src/index.js
+++ src/index.js
@@ -22,13 +22,15 @@ function checksExistsUserAccount(request, response, next) {
   next();
 }
 
+function isUserExists(username) {
+  return users.some(user => user.username === username);
+}
+
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  const isUserAlreadyExists = users.find(user => user.username === username);
-
-  if(!isUserAlreadyExists) {
-    response.status(400).json({ error: "User with this username already exists" });
+  if(isUserExists(username)) {
+    response.status(400).json({  error: 'This user already exists' });
   }
 
   const newUser = {
@@ -75,19 +77,58 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { username } = request.headers;
   const { title, deadline } = request.body;
 
-  response.status(200).send();
+  const user = users.find(user => user.username === username);
+  
+  const userTodoUpdated = user.todos.map(todo => {
+    if(todo.id === taskId) {
+      return {
+        ...todo,
+        title,
+        deadline: new Date(deadline),
+      };
+    }
+
+    return todo;
+  });
+
+  user.todos.splice(0, user.todos.length);
+  user.todos.push(...userTodoUpdated);
+
+  response.status(200).json(users);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const { username } = request.headers;
   const taskId = request.params.id;
+
+  const user = users.find(user => user.username === username);
+
+  const todoUserUpdated = user.todos.map(todo => {
+    if(todo.id === taskId) {
+      return {
+        ...todo,
+        done: true,
+      };
+    }
+    return todo;
+  });
+
+  user.todos.splice(0, user.todos.length);
+  user.todos.push(...todoUserUpdated);
   
-  response.status(200).send();
+  response.status(200).json(users);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const taskId = request.params.id;
   const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  const isTodoExists = user.todos.some(todo => todo.id === taskId);
+
+  if(!isTodoExists) return response.status(400).send();
+
+  user.todos.filter(todo => todo.id !== taskId);
 
   response.status(200).send();
 });
